9eb0ba0c0693cdcee1844b01d84edba16f557369
Marek Ventur <marekventur@gmail.com>
2012-07-29 13:42:01 +0100

    Youtube Scrobble detection ftw

--- background.js
+++ background.js
@@ -132,3 +132,86 @@ chrome.extension.onConnect.addListener(function(port) {
         updateUserContextHandler = null;
     });
 });
+
+// YouTube scrobbler
+
+//http://s.youtube.com/s?et=14.982&fvid=RrDHrwLUtvk&vw=640&st=2.941&rendering=software&vh=360&decoding=software&sw=1.0&nsidf=20&iframe=1&hbd=4985297&tsphab=1&vid=IBPCvp8x_1tPnFD253li1f9Y0WIakI2XC&vq=auto&tspne=0&bc=1779621&len=14.982&tspfdt=159&nsivbblmin=6.000&h=377&volume=100&hbt=1.990&ns=yt&docid=4JCrmVBtJ4E&nsivbblmean=391918.183&md=1&fmt=34&el=embedded&rmkt=1&nsiabblmax=120365.000&hasstoryboard=1&nsiabblmin=33.000&fexp=901601,913602,924700,909414,922600,922401,914058,913547,920706,907344,907217,924402,915507,924500,911637,902518,900816,904721,919804,921602,912706&nsivbblc=459&sd=BADC21E22HH1343330556567349&w=670&nsiabblmean=91769.526&framer=http%3A%2F%2Fwww.google.com%2Fchrome%2Fintl%2Fen%2Fmore%2Fspeed.html&tpmt=18&nsiabblc=458&fs=0&cfps=14.911463187325257&screenw=1280&hl=en_US&screenh=1024&playerw=670&eurl=http%3A%2F%2Fwww.google.com%2Fchrome%2Fintl%2Fen%2Fmore%2Fspeed.html&playerh=405&scoville=1&pd=0.600&mos=0&ptk=google&nsivbblmax=681826.000&rt=83.958&plid=AATFwISfVLOfeeQ2
+
+var filter = {
+    urls: ['http://s.youtube.com/s*']
+  };
+var notification;
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    url = parseURL(details.url);
+    console.log(url);
+    console.log('len', url.params.len);
+    console.log('pos', url.params.et);
+    console.log('id', url.params.docid);
+
+    getTitle(url.params.docid, function(t){
+        notification = webkitNotifications.createNotification(
+          'icon.png',  // icon url - can be relative
+          t,  // notification title
+          url.params.et + ' of ' + url.params.len  // notification body text
+        );
+        notification.show();
+    });
+
+}, filter);
+
+// This function creates a new anchor element and uses location
+// properties (inherent) to get the desired URL data. Some String
+// operations are used (to normalize results across browsers).
+ 
+function parseURL(url) {
+    var a =  document.createElement('a');
+    a.href = url;
+    return {
+        source: url,
+        protocol: a.protocol.replace(':',''),
+        host: a.hostname,
+        port: a.port,
+        query: a.search,
+        params: (function(){
+            var ret = {},
+                seg = a.search.replace(/^\?/,'').split('&'),
+                len = seg.length, i = 0, s;
+            for (;i<len;i++) {
+                if (!seg[i]) { continue; }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        })(),
+        file: (a.pathname.match(/\/([^\/?#]+)$/i) || [,''])[1],
+        hash: a.hash.replace('#',''),
+        path: a.pathname.replace(/^([^\/])/,'/$1'),
+        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [,''])[1],
+        segments: a.pathname.replace(/^\//,'').split('/')
+    };
+}
+
+
+
+
+function getTitle(id, callback) {
+
+
+    $.get('https://gdata.youtube.com/feeds/api/videos/'+id+'?v=2&alt=json', function(data) {
+        console.log(data);
+        media = data.entry['media$group']['media$category'];
+        found = false;
+        len = media.length;
+        for (i=0;i<len;i++) {
+            if (media[i].label == 'Music') {
+                found = true;
+            }
+        }
+        if (found) {
+            title = data.entry.title['$t'];
+            console.log(title);
+            callback(title);
+        }
+    })
+}
